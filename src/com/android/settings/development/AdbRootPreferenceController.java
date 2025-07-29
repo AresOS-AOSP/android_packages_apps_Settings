24af329ff7bf6c9facdd8fc86e132e46ce2629dd
ChrisCatto <chriscatto@proton.me>
2025-07-29 11:54:04 +0000

    Settings: Make adb root available on user builds as well
    Change-Id: I737aec999dfd454fb82b1831316ef01b5795baea
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- src/com/android/settings/development/AdbRootPreferenceController.java
+++ src/com/android/settings/development/AdbRootPreferenceController.java
@@ -50,7 +50,8 @@ public class AdbRootPreferenceController extends DeveloperOptionsPreferenceContr
 
     @Override
     public boolean isAvailable() {
-        return mADBRootService.isSupported();
+        // return mADBRootService.isSupported();
+        return true;
     }
 
     @Override
