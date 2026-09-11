Lunaris-AOSP/packages_apps_Settings

Settings: Implement haptic feedback on homepage preference selection
Change-Id: Ie2497e5ee37482986d1e7730376aabf9803a56e4

src/com/android/settings/homepage/TopLevelSettings.java
@@ -61,6 +61,8 @@ import com.android.settingslib.drawer.Tile;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.SettingsThemeHelper;
 
+import com.android.internal.util.android.VibrationUtils;
+
 import java.util.List;
 
 @SearchIndexable(forTarget = MOBILE)
@@ -127,6 +129,9 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference != null && preference.getKey() != null) {
+            VibrationUtils.triggerVibration(getContext(), 3);
+        }
         if (isDuplicateClick(preference)) {
             return true;
         }

AresOS-AOSP/android_packages_apps_Settings

Signed-off-by: Ghosuto <clash.raja10@gmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -58,6 +58,8 @@ import com.android.settingslib.drawer.Tile;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.SettingsThemeHelper;
 
+import com.android.internal.util.crdroid.VibrationUtils;
+
 import java.util.List;
 
 @SearchIndexable(forTarget = MOBILE)
@@ -124,6 +126,9 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference != null && preference.getKey() != null) {
+            VibrationUtils.triggerVibration(getContext(), 3);
+        }
         if (isDuplicateClick(preference)) {
             return true;
         }